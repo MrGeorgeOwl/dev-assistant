355bf873c820b1883728f24cc486ff8906515c69
Heorhi Parkhomenka <parhomenkogeorge@gmail.com>
2024-04-04 11:36:32 +0200

    split main logic and controllers

---
+++ controllers.py
@@ -0,0 +1,61 @@
+import os
+import json
+
+import requests
+
+
+MODEL = "codellama"
+
+
+def generate(args):
+    response = _ask_ollama(args.prompt)
+    response_lines = _process_ollama_response(response)
+    _write_output(args.output, response_lines)
+
+
+def review(args):
+    if not os.path.exists(args.file):
+        raise ValueError("provided file with code should exist to review")
+
+    with open(args.file) as f:
+        code = "\n".join(f.readlines())
+
+    response = _ask_ollama("\n".join((args.prompt, code)))
+    response_lines = _process_ollama_response(response)
+
+    _write_output("", response_lines)
+
+
+def ask(args):
+    response = _ask_ollama(args.prompt)
+    response_lines = _process_ollama_response(response)
+
+    _write_output("", response_lines)
+
+
+def _ask_ollama(prompt: str) -> requests.Response:
+    data = {
+        "model": MODEL,
+        "prompt": prompt,
+        "stream": False,
+    }
+    return requests.post("http://localhost:11434/api/generate", json=data)
+
+
+def _process_ollama_response(response: requests.Response) -> list[str]:
+    jsons = response.text.split("\n")
+    return [json.loads(j)["response"] for j in jsons if j != ""]
+
+
+def _write_output(output_dest: str, lines: list[str]) -> None:
+    output = "".join(lines)
+    if not output_dest:
+        print(output)
+        return
+
+    with open(output_dest, "w") as f:
+        if "```" in output:
+            f.write(output.split("```")[1])
+            return
+
+        f.write(output)
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 #! env/bin/python3
 
-import os
-import json
 import argparse
 
-import requests
+import controllers
 
-MODEL = "codellama"
+
+def _define_argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(help="possible methods to use:")
+    _define_generator(subparsers)
+    _define_reviewer(subparsers)
+    _define_asker(subparsers)
+
+    return parser
 
 
 def _define_generator(subparsers):
@@ -27,7 +33,7 @@ def _define_generator(subparsers):
         help="destination to output file",
         default="",
     )
-    generator.set_defaults(func=generate)
+    generator.set_defaults(func=controllers.generate)
 
 
 def _define_reviewer(subparsers):
@@ -48,7 +54,7 @@ def _define_reviewer(subparsers):
         help="destination to file with code to review",
         default="",
     )
-    reviewer.set_defaults(func=review)
+    reviewer.set_defaults(func=controllers.review)
 
 
 def _define_asker(subparsers):
@@ -60,71 +66,7 @@ def _define_asker(subparsers):
         required=True,
         help="prompt to be used for ollama model",
     )
-    asker.set_defaults(func=ask)
-
-
-def _define_argparser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(help="possible methods to use:")
-    _define_generator(subparsers)
-    _define_reviewer(subparsers)
-    _define_asker(subparsers)
-
-    return parser
-
-
-def generate(args):
-    response = _ask_ollama(args.prompt)
-    response_lines = _process_ollama_response(response)
-    _write_output(args.output, response_lines)
-
-
-def review(args):
-    if not os.path.exists(args.file):
-        raise ValueError("provided file with code should exist to review")
-
-    with open(args.file) as f:
-        code = "\n".join(f.readlines())
-
-    response = _ask_ollama("\n".join((args.prompt, code)))
-    response_lines = _process_ollama_response(response)
-
-    _write_output("", response_lines)
-
-
-def ask(args):
-    response = _ask_ollama(args.prompt)
-    response_lines = _process_ollama_response(response)
-
-    _write_output("", response_lines)
-
-
-def _ask_ollama(prompt: str) -> requests.Response:
-    data = {
-        "model": MODEL,
-        "prompt": prompt,
-        "stream": False,
-    }
-    return requests.post("http://localhost:11434/api/generate", json=data)
-
-
-def _write_output(output_dest: str, lines: list[str]) -> None:
-    output = "".join(lines)
-    if not output_dest:
-        print(output)
-        return
-
-    with open(output_dest, "w") as f:
-        if "```" in output:
-            f.write(output.split("```")[1])
-            return
-
-        f.write(output)
-
-
-def _process_ollama_response(response: requests.Response) -> list[str]:
-    jsons = response.text.split("\n")
-    return [json.loads(j)["response"] for j in jsons if j != ""]
+    asker.set_defaults(func=controllers.ask)
 
 
 def main(args):
